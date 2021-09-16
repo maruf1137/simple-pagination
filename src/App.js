aae25b7ed21b576742ba7b5082d93058693e2ee8
maruf1137 <mmaruf1137@gmail.com>
2021-09-16 10:06:52 +0600

    files added

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
+function App() {
+  const { loading, data } = useFetch();
+  const [page, setpage] = useState(0);
+  const [followers, setfollowers] = useState([]);
+
+  useEffect(() => {
+    if (loading) return;
+    setfollowers(data[page]);
+  }, [loading, page]);
+
+  const handlePage = (index) => {
+    setpage(index);
+  };
+
+  const nextPage = () => {
+    setpage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+
+  const prevPage = () => {
+    setpage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+
+  return (
+    <main>
+      <section className="section-title">
+        <h1>{loading ? "loading..." : "pagination"}</h1>
+        <div className="underline"></div>
+      </section>
+      <section className="followers">
+        <div className="container">
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
+        </div>
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? "active-btn" : null}`}
+                  onClick={() => handlePage(index)}
+                >
+                  {index}
+                </button>
+              );
+            })}
+            <button className="next-btn" onClick={nextPage}>
+              next
+            </button>
+          </div>
+        )}
+      </section>
+    </main>
+  );
+}
+
+export default App;
